4fa5ad2da32927fac6b367858dbc4addd8196d67
Florin Pop <popflorin1705@yahoo.com>
2017-03-01 16:52:16 +0200

    transition head + js + images

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
-$(document).ready(function(){
+var toggleHeads;
 
+$(document).ready(function(){
+    var head1 = $('.driver-head-1');
+    var head2 = $('.driver-head-2');
+
+    toggleHeads = function() {
+        head1.toggleClass('hide');
+        head2.toggleClass('hide');
+    }
 });
