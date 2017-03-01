270e927e5147563c1f5a2c5b220056608302890f
Florin Pop <popflorin1705@yahoo.com>
2017-03-01 18:28:36 +0200

    javascript position top instead of css

--- js/script.js
+++ js/script.js
@@ -8,4 +8,29 @@ $(document).ready(function(){
         head1.toggleClass('hide');
         head2.toggleClass('hide');
     }
+
+    // get all info_boxes
+    var info_boxes = $('.animation-container .info-box');
+
+    // get all images
+    var imgs = $('.animation-container > img');
+
+    // set top position to data-top attribute
+    setDOMArrayToPos(info_boxes);
+    setDOMArrayToPos(imgs);
 });
+
+function setDOMArrayToPos(arr) {
+    // loop through all boxes
+    arr.each((idx, item) => {
+
+        // convert DOM element to jQuery obj
+        item = $(item);
+
+        // get the data-top attribute
+        var topPos = item.data('top') + 'px';
+
+        // set the value to it's top attribute
+        item.css('top', topPos);
+    });
+}
